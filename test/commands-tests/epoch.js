45c84b2f405ff8a05bab9419d2e8784809a54641
Martin Grigorov <maptuhec@gmail.com>
2018-12-13 14:07:45 +0200

    Add more unit test to epoch command

--- test/commands-tests/epoch.js
+++ test/commands-tests/epoch.js
@@ -1,14 +1,17 @@
 const chai = require('chai');
 let chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-const assert = chai.assert;
 const execute = require('../../cli-commands/utils.js').aeprojectExecute;
 const dockerPs = require('../utils').dockerPs;
 const constants = require('../constants.json')
 const fs = require('fs-extra')
+const epochConfig = require('../../cli-commands/aeproject-epoch/config.json')
+const utils = require('../../cli-commands/utils')
 let executeOptions = {
 	cwd: process.cwd() + constants.epochTestsFolderPath
 };
+chai.use(chaiAsPromised);
+const assert = chai.assert;
+const defaultWallets = epochConfig.defaultWallets
 
 
 describe('Aeproject Epoch', () => {
@@ -29,6 +32,26 @@ describe('Aeproject Epoch', () => {
 		await execute(constants.cliCommands.EPOCH, [constants.cliCommandsOptions.STOP], executeOptions)
 		let running = await dockerPs();
 		assert.isNotTrue(running, "Epoch wasn't stopped properly");
+	})
+
+	it('Should check if the wallets are funded', async () => {
+		await execute(constants.cliCommands.EPOCH, [], executeOptions)
+
+		let client = await utils.getClient(epochConfig.config.host);
+		for (let wallet in defaultWallets) {
+			let recipientBalanace = await client.balance(defaultWallets[wallet].publicKey)
+			assert.isAbove(Number(recipientBalanace), 0, `${defaultWallets[wallet].publicKey} balance is not greater than 0`);
+		}
+	})
+
+	it('Should check if the wallets are funded with the exact amount', async () => {
+		await execute(constants.cliCommands.EPOCH, [], executeOptions)
+
+		let client = await utils.getClient(epochConfig.config.host);
+		for (let wallet in defaultWallets) {
+			let recipientBalanace = await client.balance(defaultWallets[wallet].publicKey)
+			assert.equal(Number(recipientBalanace), epochConfig.config.amountToFund, `${defaultWallets[wallet].publicKey} balance is not greater than 0`);
+		}
 	})
 
 	// TODO add test seeing that the wallets were funded
