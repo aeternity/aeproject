55e25860b4644d0d7bcaec3c14e9f1676a087582
George Spasov <spasov05203@gmail.com>
2018-12-20 12:18:45 +0200

    Wrong error message

--- cli-commands/aeproject-init/artifacts/exampleTests.js
+++ cli-commands/aeproject-init/artifacts/exampleTests.js
@@ -52,7 +52,7 @@ describe('Example Contract', () => {
       }
     });
 
-    assert.isFulfilled(deployPromise, 'Could not deploy the Restricted Smart Contract'); // Check it is deployed
+    assert.isFulfilled(deployPromise, 'Could not deploy the ExampleContract Smart Contract'); // Check it is deployed
   })
 
 })
