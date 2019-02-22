a8d9f6f17d5509f42fb9bfb44b89a4fcbad18dc6
Vladdy <ivanov.vladdy@gmail.com>
2019-02-22 15:52:26 +0200

    Move constants to json config from the test file

--- test/commands-tests/contracts.js
+++ test/commands-tests/contracts.js
@@ -6,18 +6,12 @@ const assert = chai.assert;
 const execute = require('../../cli-commands/utils').forgaeExecute;
 const timeout = require('../../cli-commands/utils').timeout;
 const constants = require('../constants.json');
+const contractsConstants = require('../../cli-commands/forgae-contracts/contracts-constants.json');
 const {
   spawn
 } = require('promisify-child-process');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-const LOG_FILE = './contractLogFile.log';
-const LOCALHOST_SUCCESS = 'Contracts web Aepp is running';
-const UPDATE_FLAG_CHECK_CONDITION = 'Installing Contracts web Aepp';
-const FORGAE_CLI_COMMAND = 'forgae';
-const STARTING_AEPP_TIMEOUT = 100000;
-const SPECIFIC_LOCAL_NODE_URL = "http://localhost:3002/";
 
 let executeOptions = {
   cwd: process.cwd() + constants.testTestsFolderPath
@@ -33,39 +27,39 @@ describe('ForgAE contracts', () => {
   });
 
   it('should execute contracts cli command correctly', async function () {
-    const logStream = fs.createWriteStream(LOG_FILE, { flags: 'a' });
-    contractsResult = spawn(FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.IGNORE_OPENING], {});
+    const logStream = fs.createWriteStream(contractsConstants.LOG_FILE, { flags: 'a' });
+    contractsResult = spawn(contractsConstants.FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.IGNORE_OPENING], {});
     contractsResult.stdout.pipe(logStream);
-    await timeout(STARTING_AEPP_TIMEOUT);
-    const logContent = fs.readFileSync(LOG_FILE, 'utf8');
-    assert.include(logContent, LOCALHOST_SUCCESS);
-    fs.removeSync(LOG_FILE);
+    await timeout(contractsConstants.STARTING_AEPP_TIMEOUT);
+    const logContent = fs.readFileSync(contractsConstants.LOG_FILE, 'utf8');
+    assert.include(logContent, contractsConstants.LOCALHOST_SUCCESS);
+    fs.removeSync(contractsConstants.LOG_FILE);
   });
 
   it('should execute contracts cli command with update parameter correctly', async function () {
-    const logStream = fs.createWriteStream(LOG_FILE, { flags: 'a' });
-    contractsResult = spawn(FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.UPDATE, constants.cliCommandsOptions.IGNORE_OPENING], {});
+    const logStream = fs.createWriteStream(contractsConstants.LOG_FILE, { flags: 'a' });
+    contractsResult = spawn(contractsConstants.FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.UPDATE, constants.cliCommandsOptions.IGNORE_OPENING], {});
     contractsResult.stdout.pipe(logStream);
-    await timeout(STARTING_AEPP_TIMEOUT);
-    const logContent = fs.readFileSync(LOG_FILE, 'utf8');
-    assert.include(logContent, UPDATE_FLAG_CHECK_CONDITION);
-    assert.include(logContent, LOCALHOST_SUCCESS);
-    fs.removeSync(LOG_FILE);
+    await timeout(contractsConstants.STARTING_AEPP_TIMEOUT);
+    const logContent = fs.readFileSync(contractsConstants.LOG_FILE, 'utf8');
+    assert.include(logContent, contractsConstants.UPDATE_FLAG_CHECK_CONDITION);
+    assert.include(logContent, contractsConstants.LOCALHOST_SUCCESS);
+    fs.removeSync(contractsConstants.LOG_FILE);
   });
 
   it('should connect the contracts aepp to the specified nodeUrl', async function () {
-    const logStream = fs.createWriteStream(LOG_FILE, { flags: 'a' });
-    contractsResult = spawn(FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.NODE_URL, SPECIFIC_LOCAL_NODE_URL, constants.cliCommandsOptions.IGNORE_OPENING], {});
+    const logStream = fs.createWriteStream(contractsConstants.LOG_FILE, { flags: 'a' });
+    contractsResult = spawn(contractsConstants.FORGAE_CLI_COMMAND, [constants.cliCommands.CONTRACTS, constants.cliCommandsOptions.NODE_URL, contractsConstants.SPECIFIC_LOCAL_NODE_URL, constants.cliCommandsOptions.IGNORE_OPENING], {});
     contractsResult.stdout.pipe(logStream);
-    await timeout(STARTING_AEPP_TIMEOUT);
-    const logContent = fs.readFileSync(LOG_FILE, 'utf8');
-    assert.include(logContent, SPECIFIC_LOCAL_NODE_URL);
-    assert.include(logContent, LOCALHOST_SUCCESS);
-    fs.removeSync(LOG_FILE);
+    await timeout(contractsConstants.STARTING_AEPP_TIMEOUT);
+    const logContent = fs.readFileSync(contractsConstants.LOG_FILE, 'utf8');
+    assert.include(logContent, contractsConstants.SPECIFIC_LOCAL_NODE_URL);
+    assert.include(logContent, contractsConstants.LOCALHOST_SUCCESS);
+    fs.removeSync(contractsConstants.LOG_FILE);
   });
 
   after(async function () {
-    await execute(constants.cliCommands.NODE, [constants.cliCommandsOptions.STOP], executeOptions)
+    await execute(constants.cliCommands.NODE, [constants.cliCommandsOptions.STOP], executeOptions);
     fs.removeSync(`.${constants.testTestsFolderPath}`);
     await exec('kill $(lsof -t -i:8080)');
   })
