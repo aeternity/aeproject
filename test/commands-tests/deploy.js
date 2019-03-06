ea7b1d492d986e3f7235c7c1a966b7f34eebd147
Vladdy <ivanov.vladdy@gmail.com>
2019-03-06 16:37:47 +0200

    Update sdk version to 2.3.2 and resolve unit test problems

--- test/commands-tests/deploy.js
+++ test/commands-tests/deploy.js
@@ -9,6 +9,10 @@ const fs = require('fs-extra')
 let executeOptions = {
 	cwd: process.cwd() + constants.deployTestsFolderPath
 };
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
 const Deployer = require("./../../cli-commands/forgae-deploy/forgae-deployer")
 describe('ForgAE Deploy', () => {
 	const secretKey = "bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca"
@@ -129,12 +133,26 @@ describe('ForgAE Deploy', () => {
 			assert.include(result, expectedDeployResult)
 		})
 
-		it('with network and secret on test network', async () => {
-			let testSecretKey = "123a422a7d60a14559f65d64e1762cce04706844d8249e9ca708a4f6d99c8aed3c95e234526e2baec1f800ffc979b3fea0e2006a5336aa66fab7f17ec0e3b319"
-			let result = await execute(constants.cliCommands.DEPLOY, ["-n", "testnet", "-s", testSecretKey], executeOptions)
+    it('with network and secret on test network', async () => {
+      let testSecretKey = constants.privateKeyTestnetDeploy;
+      let result = '';
 
-			assert.include(result, expectedDeployResult)
-		})
+      const mainForgaeProjectDir = process.cwd();
+      process.chdir(executeOptions.cwd);
+
+      await exec(`npm link ${mainForgaeProjectDir}`);
+
+      try {
+        result = await execute(constants.cliCommands.DEPLOY, ["-n", "testnet", "-s", testSecretKey], executeOptions);
+      } catch (err) {
+        console.log(err);
+        console.log(err.stdout.toString('utf8'));
+      }
+
+      process.chdir(mainForgaeProjectDir);
+
+      assert.include(result, expectedDeployResult)
+    });
 
 
 		it('with invalid network arguments', async () => {
--- test/commands-tests/init.js
+++ test/commands-tests/init.js
@@ -7,81 +7,81 @@ const constants = require('../constants.json')
 const packageJson = require('../../package.json')
 
 let executeOptions = {
-	cwd: process.cwd() + constants.initTestsFolderPath
+  cwd: process.cwd() + constants.initTestsFolderPath
 };
 
 chai.use(chaiFiles);
 
 describe('ForgAE Init', () => {
-	before(async () => {
-		fs.ensureDirSync(`.${constants.initTestsFolderPath}`)
-	})
+  before(async () => {
+    fs.ensureDirSync(`.${constants.initTestsFolderPath}`)
+  })
 
-	it('Should init project successfully', async () => {
-		await execute(constants.cliCommands.INIT, [], executeOptions)
+  it('Should init project successfully', async () => {
+    await execute(constants.cliCommands.INIT, [], executeOptions)
 
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageJson}`), "package.json doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageLockJson}`), "package-lock.json doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerComposeYml}`), "docker-compose.yml doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.testContractPath}`), "test contract doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.deployScriptsPath}`), "deploy scripts doesn't exists");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.contractsPath}`), "example contract doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.nodeModules}`), "node modules folder doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerEntryPoint}`), "docker entrypoint.sh doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode1}`), "docker node node1 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode2}`), "docker node node2 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode3}`), "docker node node3 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerHealthCheck}`), "docker healtcheck.sh doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxCors}`), "docker nginx-cors.conf doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxDefault}`), "docker nginx-default doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxWs}`), "docker nginx-ws doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerKeys}`), "docker keys folder doesn't exist");
-	})
-
-
-	it('Should update project successfully', async () => {
-		//Arrange
-		const editedContent = "edited content"
-		await execute(constants.cliCommands.INIT, [], executeOptions)
-
-		//Act
-		fs.writeFile(executeOptions.cwd + constants.testsFiles.dockerComposeYml, editedContent)
-
-		await execute(constants.cliCommands.INIT, ["--update"], executeOptions)
-
-		// //assert
-		let editedDockerComposeYml = fs.readFileSync(executeOptions.cwd + constants.testsFiles.dockerComposeYml, 'utf8')
-		const forgaeVersion = packageJson.version
-		const sdkVersion = packageJson.dependencies['@aeternity/aepp-sdk']
-		const projectPackageJson = require("./initTests/package.json")
-
-		const forgaeVersionInProject = projectPackageJson.dependencies['forgae']
-		const sdkVersionInProject = projectPackageJson.dependencies['@aeternity/aepp-sdk']
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageJson}`), "package.json doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageLockJson}`), "package-lock.json doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerComposeYml}`), "docker-compose.yml doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.testContractPath}`), "test contract doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.deployScriptsPath}`), "deploy scripts doesn't exists");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.contractsPath}`), "example contract doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.nodeModules}`), "node modules folder doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerEntryPoint}`), "docker entrypoint.sh doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode1}`), "docker node node1 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode2}`), "docker node node2 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode3}`), "docker node node3 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerHealthCheck}`), "docker healtcheck.sh doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxCors}`), "docker nginx-cors.conf doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxDefault}`), "docker nginx-default doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxWs}`), "docker nginx-ws doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerKeys}`), "docker keys folder doesn't exist");
+  })
 
 
-		assert.notEqual(editedDockerComposeYml, editedContent)
-		assert.equal(forgaeVersion, forgaeVersionInProject, "Forgae version is not updated properly")
-		assert.equal(sdkVersion, sdkVersionInProject, "sdk version is not updated properly")
+  it('Should update project successfully', async () => {
+    //Arrange
+    const editedContent = "edited content"
+    await execute(constants.cliCommands.INIT, [], executeOptions)
 
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageJson}`), "package.json doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageLockJson}`), "package-lock.json doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerComposeYml}`), "docker-compose.yml doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.testContractPath}`), "test contract doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.deployScriptsPath}`), "deploy scripts doesn't exists");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.contractsPath}`), "example contract doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.nodeModules}`), "node modules folder doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerEntryPoint}`), "docker entrypoint.sh doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode1}`), "docker node node1 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode2}`), "docker node node2 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode3}`), "docker node node3 doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerHealthCheck}`), "docker healtcheck.sh doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxCors}`), "docker nginx-cors.conf doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxDefault}`), "docker nginx-default doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxWs}`), "docker nginx-ws doesn't exist");
-		assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerKeys}`), "docker keys folder doesn't exist");
-	})
+    //Act
+    fs.writeFile(executeOptions.cwd + constants.testsFiles.dockerComposeYml, editedContent)
 
-	after(async () => {
-		fs.removeSync(`.${constants.initTestsFolderPath}`);
-	})
+    await execute(constants.cliCommands.INIT, ["--update"], executeOptions)
+
+    // //assert
+    let editedDockerComposeYml = fs.readFileSync(executeOptions.cwd + constants.testsFiles.dockerComposeYml, 'utf8')
+    const forgaeVersion = packageJson.version
+    const sdkVersion = packageJson.dependencies['@aeternity/aepp-sdk']
+    const projectPackageJson = require("./initTests/package.json")
+
+    const forgaeVersionInProject = projectPackageJson.dependencies['forgae']
+    const sdkVersionInProject = projectPackageJson.dependencies['@aeternity/aepp-sdk']
+
+
+    assert.notEqual(editedDockerComposeYml, editedContent)
+    assert.equal(forgaeVersion, forgaeVersionInProject, "Forgae version is not updated properly")
+    assert.equal(sdkVersion, sdkVersionInProject, "sdk version is not updated properly")
+
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageJson}`), "package.json doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.packageLockJson}`), "package-lock.json doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerComposeYml}`), "docker-compose.yml doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.testContractPath}`), "test contract doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.deployScriptsPath}`), "deploy scripts doesn't exists");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.contractsPath}`), "example contract doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.nodeModules}`), "node modules folder doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerEntryPoint}`), "docker entrypoint.sh doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode1}`), "docker node node1 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode2}`), "docker node node2 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockernodeNode3}`), "docker node node3 doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerHealthCheck}`), "docker healtcheck.sh doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxCors}`), "docker nginx-cors.conf doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxDefault}`), "docker nginx-default doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerNginxWs}`), "docker nginx-ws doesn't exist");
+    assert.isTrue(fs.existsSync(`${executeOptions.cwd}${constants.testsFiles.dockerKeys}`), "docker keys folder doesn't exist");
+  })
+
+  after(async () => {
+    fs.removeSync(`.${constants.initTestsFolderPath}`);
+  })
 })
