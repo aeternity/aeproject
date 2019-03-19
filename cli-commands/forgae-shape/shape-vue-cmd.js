4b4bd4be991ed35d88dff47a70a5fa6c55f8bb7d
Vladdy <ivanov.vladdy@gmail.com>
2019-03-19 13:58:01 +0200

    Add preparing vue project logic on shape command running

--- cli-commands/forgae-shape/shape-vue-cmd.js
+++ cli-commands/forgae-shape/shape-vue-cmd.js
@@ -1,24 +1,50 @@
+const util = require('util');
+const childProcess = util.promisify(require('child_process').exec);
 
 const utils = require('./../utils');
 const forgaeExec = utils.forgaeExecute;
 const exec = utils.execute;
 
+const fs = require('fs-extra');
+
 const vueRepo = require('./constants.json').repos.vue;
+const constants = require('../forgae-init/constants.json');
+const copyFileOrDir = require('../utils').copyFileOrDir;
+const vueWebProjectPath = './aepp-forgae-shape-vue';
+
+let self;
 
 class VueSubCommand {
-    constructor(){
-        this.name = 'vue'
-    }
+	constructor() {
+		this.name = 'vue'
+		self = this;
+	}
 
-    async run () {
+	async run() {
+		console.log('Preparation of a ready-to-use aepp with predefined Vue.js frontend framework and forgae integration');
 
-        await forgaeExec('init');
-        await exec('git', 'clone', [vueRepo]);
+		await forgaeExec('init');
 
-        console.log('Vue project successfully initialized.');
-    } 
+		await self.prepareVueProject();
+
+		console.log('Vue project successfully initialized.');
+	}
+
+	async prepareVueProject() {
+		fs.removeSync(constants.contractFileDestination);
+		const fileSource = `${__dirname}${constants.artifactsDir}/${constants.contractTemplateFile}`;
+		copyFileOrDir(fileSource, constants.contractFileDestination);
+
+		await exec('git', 'clone', [vueRepo]);
+
+		const currentDir = process.cwd();
+		process.chdir(vueWebProjectPath);
+
+		await childProcess('npm install');
+		process.chdir(currentDir);
+	}
 }
 
-const subCommand = new VueSubCommand(); 
+const subCommand = new VueSubCommand();
 
 module.exports = subCommand;
