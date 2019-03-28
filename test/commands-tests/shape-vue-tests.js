421ed8b30071dcd017bdd0c99bc53a3b604ff8dc
Aleksey <alexeitcekov@abv.bg>
2019-03-28 14:20:48 +0200

    Command shape vue #90 (#125)
    * init commit, create command
    
    * temp commit
    
    * implement command logic
    
    * implement shape vue command tests
    
    * remove 'only' from tests
    
    * Add preparing vue project logic on shape command running (#122)
    
    * Add preparing vue project logic on shape command running
    
    * Add shape option to init command which define the type of created contract and relevant files
    
    * Add documentation for the shape command
    
    * Remove shape optional parameter, export create forgae project structure function and use it
    
    * Resolve shape command test. Replace ExampleContract with ToDoManager
    
    * Resolve shape command test. Replace ExampleContract with ToDoManager

---
+++ test/commands-tests/shape-vue-tests.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const chaiFiles = require('chai-files');
+const assert = chai.assert;
+const forgaeExecute = require('../../cli-commands/utils.js').forgaeExecute;
+const fs = require('fs-extra');
+const path = require('path');
+
+chai.use(chaiFiles);
+
+const constants = require('../constants.json');
+const cliCmds = constants.cliCommands;
+const cliSubCmds = constants.cliSubCommands;
+
+const testWorkingDir = constants.shapeVueTestsFolderPath;
+
+let cwd = process.cwd();
+
+describe('ForgAE Shape Vue', async () => {
+
+	let workingDir;
+
+	before(async () => {
+		workingDir = path.join(cwd, testWorkingDir);
+
+		fs.ensureDirSync(workingDir);
+		process.chdir(workingDir);
+	});
+
+	it('Should init shape vue project successfully', async () => {
+		await forgaeExecute(cliCmds.SHAPE, [cliSubCmds.VUE]);
+
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.packageJson)), "package.json doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.packageLockJson)), "package-lock.json doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerComposeYml)), "docker-compose.yml doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.toDoTestContractPath)), "test contract doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.deployScriptsPath)), "deploy scripts doesn't exists");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.contractsAeppSettings)), "contracts aepp settings file doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.nodeModules)), "node modules folder doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerEntryPoint)), "docker entrypoint.sh doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockernodeNode1)), "docker node node1 doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockernodeNode2)), "docker node node2 doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockernodeNode3)), "docker node node3 doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerHealthCheck)), "docker healtcheck.sh doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerNginxCors)), "docker nginx-cors.conf doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerNginxDefault)), "docker nginx-default doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerNginxWs)), "docker nginx-ws doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.dockerKeys)), "docker keys folder doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.forgaeStore)), "forgae store folder doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.vueWebProjectPath)), "vue web project folder doesn't exist");
+		assert.isTrue(fs.existsSync(path.join(workingDir, constants.testsFiles.toDoContract)), "ToDo Manager contract doesn't exist");
+	});
+
+	after(async () => {
+		// delete test cwd
+		fs.removeSync(workingDir);
+
+		process.chdir(cwd);
+	})
+});
