bfaff2589006c3afbeb7f9842ddc02f1ab883187
Aleksey <alexeitcekov@abv.bg>
2019-05-13 17:05:40 +0300

    add test for  'compiler cmd with subcmd '--compiler' and test for cmd 'node' with sub cmd '--only'

--- cli-commands/commands.js
+++ cli-commands/commands.js
@@ -19,115 +19,116 @@ const init = require('./forgae-init/init.js');
 const testConfig = require('./forgae-test/test.js');
 const node = require('./forgae-node/node.js');
 const deploy = require('./forgae-deploy/deploy.js');
-const config = require('./utils').config;
+const config = require('./config.json');
 const history = require('./forgae-history/log-store-service');
 const printReportTable = require('./forgae-history/utils').printReportTable;
 const contracts = require('./forgae-contracts/forgae-contracts.js');
 const shape = require('./forgae-shapes/shape-commander');
 
 const addInitOption = (program) => {
-	program
-		.command('init')
-		.description('Initialize ForgAE')
-		.option('--update [update]', 'Update project files')
-		.action(async (option) => {
-			await init.run(option.update);
-		})
+    program
+        .command('init')
+        .description('Initialize ForgAE')
+        .option('--update [update]', 'Update project files')
+        .action(async (option) => {
+            await init.run(option.update);
+        })
 }
 
 const addCompileOption = (program) => {
-	program
-		.command('compile')
-		.option('-n --network [network]', 'Network to connect to', "local")
-		.option('--path [compile path]', 'Path to contract files', './contracts')
-		.description('Compile contracts')
-		.action(async (option) => {
-			await compile.run(option.path, option.network);
-		})
+    program
+        .command('compile')
+        .option('-n --network [network]', 'Network to connect to', "local")
+        .option('--path [compile path]', 'Path to contract files', './contracts')
+        .option('--compiler [compiler url]', 'Path to contract files', config.compilerUrl)
+        .description('Compile contracts')
+        .action(async (option) => {
+            await compile.run(option.path, option.network, option.compiler);
+        })
 }
 
 const addTestOption = (program) => {
-	program
-		.command('test')
-		.description('Running the tests')
-		.option('--path [tests path]', 'Path to test files', './test')
-		.action(async (options) => {
-			await testConfig.run(options.path);
-		})
+    program
+        .command('test')
+        .description('Running the tests')
+        .option('--path [tests path]', 'Path to test files', './test')
+        .action(async (options) => {
+            await testConfig.run(options.path);
+        })
 }
 
 const addNodeOption = (program) => {
-	program
-		.command('node')
-		.description('Running a local node. Without any argument node will be runned with --start argument')
-		.option('--stop', 'Stop the node')
-		.option('--start', 'Start the node')
-		.option('--only', 'Start only the node without local compiler')
-		.action(async (options) => {
-			await node.run(options);
-		})
+    program
+        .command('node')
+        .description('Running a local node. Without any argument node will be runned with --start argument')
+        .option('--stop', 'Stop the node')
+        .option('--start', 'Start the node')
+        .option('--only', 'Start only the node without local compiler')
+        .action(async (options) => {
+            await node.run(options);
+        })
 }
 
 const addDeployOption = (program) => {
-	program
-		.command('deploy')
-		.description('Run deploy script')
-		.option('--path [deploy path]', 'Path to deployment file', './deployment/deploy.js')
-		.option('-n --network [network]', 'Select network', "local")
-		.option('-s --secretKey [secretKey]', 'Wallet secretKey(privateKey)')
-		.action(async (options) => {
-			await deploy.run(options.path, options.network, options.secretKey);
-		})
+    program
+        .command('deploy')
+        .description('Run deploy script')
+        .option('--path [deploy path]', 'Path to deployment file', './deployment/deploy.js')
+        .option('-n --network [network]', 'Select network', "local")
+        .option('-s --secretKey [secretKey]', 'Wallet secretKey(privateKey)')
+        .action(async (options) => {
+            await deploy.run(options.path, options.network, options.secretKey);
+        })
 }
 
 const addHistoryOption = (program) => {
-	program
-		.command('history')
-		.description('Show deployment history info')
-		.option('--limit [limit]', 'Get last N records.', 5)
-		.action(async (options) => {
+    program
+        .command('history')
+        .description('Show deployment history info')
+        .option('--limit [limit]', 'Get last N records.', 5)
+        .action(async (options) => {
 
-			let data = history.getHistory().slice(options.limit * -1);
+            let data = history.getHistory().slice(options.limit * -1);
 
-			for (let i = 0; i < data.length; i++) {
-				printReportTable(data[i].actions);
-			}
-		})
+            for (let i = 0; i < data.length; i++) {
+                printReportTable(data[i].actions);
+            }
+        })
 }
 
 const addContractsAeppIntegrationOption = (program) => {
-	program
-		.command('contracts')
-		.description('Running a Contract web aepp locally and connect it to the spawned forgae node.')
-		.option('--nodeUrl [nodeUrl]', 'Specify the url of the local spawned node', 'http://localhost:3001/')
-		.option('--update [update]', 'Update the contracts aepp with the latest version of develop branch')
-		.option('--ignoreOpenInBrowser [ignoreOpenInBrowser]', 'Ignore browser opening')
-		.action(async (options) => {
-			await contracts.run(options);
-		})
+    program
+        .command('contracts')
+        .description('Running a Contract web aepp locally and connect it to the spawned forgae node.')
+        .option('--nodeUrl [nodeUrl]', 'Specify the url of the local spawned node', 'http://localhost:3001/')
+        .option('--update [update]', 'Update the contracts aepp with the latest version of develop branch')
+        .option('--ignoreOpenInBrowser [ignoreOpenInBrowser]', 'Ignore browser opening')
+        .action(async (options) => {
+            await contracts.run(options);
+        })
 };
 
 const addShapeOption = (program) => {
-	program
-		.command('shape')
-		.description('Initialize a web Vue project.')
-		.arguments('<type> [type]', 'Type can be Vue|vue.')
-		.action(async (options) => {
-			await shape.run(options);
-		})
+    program
+        .command('shape')
+        .description('Initialize a web Vue project.')
+        .arguments('<type> [type]', 'Type can be Vue|vue.')
+        .action(async (options) => {
+            await shape.run(options);
+        })
 };
 
 const initCommands = (program) => {
-	addInitOption(program);
-	addCompileOption(program);
-	addTestOption(program);
-	addNodeOption(program);
-	addDeployOption(program);
-	addHistoryOption(program);
-	addContractsAeppIntegrationOption(program)
-	addShapeOption(program);
+    addInitOption(program);
+    addCompileOption(program);
+    addTestOption(program);
+    addNodeOption(program);
+    addDeployOption(program);
+    addHistoryOption(program);
+    addContractsAeppIntegrationOption(program)
+    addShapeOption(program);
 }
 
 module.exports = {
-	initCommands
+    initCommands
 }
--- test/commands-tests/compile.js
+++ test/commands-tests/compile.js
@@ -9,73 +9,92 @@ const expectedCompileResultExampleContract = "ExampleContract.aes has been succe
 let expectedResult1 = "ExampleContract1.aes has been successfully compiled"
 let expectedResult2 = "ExampleContract2.aes has been successfully compiled"
 let expectedResult3 = "ExampleContract3.aes has been successfully compiled"
+let expectedResult4 = "ExampleContract.aes has not been compiled"
 let executeOptions = {
-	cwd: process.cwd() + constants.compileTestsFolderPath
+    cwd: process.cwd() + constants.compileTestsFolderPath
 };
 chai.use(chaiAsPromised);
 
 describe('ForgAE Compile', () => {
-	before(async () => {
-		fs.ensureDirSync(`.${constants.compileTestsFolderPath}`)
-		await execute(constants.cliCommands.INIT, [], executeOptions)
-		await execute(constants.cliCommands.NODE, [], executeOptions)
-	})
+    before(async () => {
+        fs.ensureDirSync(`.${ constants.compileTestsFolderPath }`)
+        await execute(constants.cliCommands.INIT, [], executeOptions)
+        await execute(constants.cliCommands.NODE, [], executeOptions)
+    })
 
-	describe('Compile', () => {
-		it('Should compile contract successfully with specif contract path', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, [constants.cliCommandsOptions.PATH, `${executeOptions.cwd}/contracts/ExampleContract.aes`])
-			assert.include(result, expectedCompileResultExampleContract)
-		})
+    describe('Compile', () => {
+        it('Should compile contract successfully with specif contract path', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, [constants.cliCommandsOptions.PATH, `${ executeOptions.cwd }/contracts/ExampleContract.aes`])
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-		it('Should compile contract successfully without path', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, [], executeOptions)
-			assert.include(result, expectedCompileResultExampleContract)
-		})
+        it('Should compile contract successfully without path', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, [], executeOptions)
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-		it('Should compile multiple contracts successfully with path', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, [constants.cliCommandsOptions.PATH, "../multipleContractsFolder"], executeOptions)
-			assert.include(result, expectedResult1)
-			assert.include(result, expectedResult2)
-			assert.include(result, expectedResult3)
-		})
+        it('Should compile multiple contracts successfully with path', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, [constants.cliCommandsOptions.PATH, "../multipleContractsFolder"], executeOptions)
+            assert.include(result, expectedResult1)
+            assert.include(result, expectedResult2)
+            assert.include(result, expectedResult3)
+        })
 
-		it('Should compile contracts with -n argument - localhost', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["-n", "local"], executeOptions)
+        it('Should compile contracts with -n argument - localhost', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["-n", "local"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-		it('Should compile contracts with -n argument - testnet ', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["-n", "testnet"], executeOptions)
+        it('Should compile contracts with -n argument - testnet ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["-n", "testnet"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
-		it('Should compile contracts with -n argument - mainnet ', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["-n", "mainnet"], executeOptions)
+            assert.include(result, expectedCompileResultExampleContract)
+        })
+        it('Should compile contracts with -n argument - mainnet ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["-n", "mainnet"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-		it('Should compile contracts with --network argument - localhost', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["--network", "local"], executeOptions)
+        it('Should compile contracts with --network argument - localhost', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--network", "local"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-		it('Should compile contracts with --network argument - testnet ', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["--network", "testnet"], executeOptions)
+        it('Should compile contracts with --network argument - testnet ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--network", "testnet"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
-		it('Should compile contracts with --netwotk argument - mainnet ', async () => {
-			let result = await execute(constants.cliCommands.COMPILE, ["--network", "mainnet"], executeOptions)
+            assert.include(result, expectedCompileResultExampleContract)
+        })
+        it('Should compile contracts with --netwotk argument - mainnet ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--network", "mainnet"], executeOptions)
 
-			assert.include(result, expectedCompileResultExampleContract)
-		})
-	})
+            assert.include(result, expectedCompileResultExampleContract)
+        })
 
-	after(async () => {
-		await execute(constants.cliCommands.NODE, [constants.cliCommandsOptions.STOP], executeOptions)
-		fs.removeSync(`.${constants.compileTestsFolderPath}`);
-	})
+        it('Should compile contracts with --compiler argument - localhost ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--compiler", "http://localhost:3080"], executeOptions)
+
+            assert.include(result, expectedCompileResultExampleContract);
+        })
+
+        it('Should compile contracts with --compiler argument - official compiler ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--compiler", "https://compiler.aepps.com"], executeOptions)
+            
+            assert.include(result, expectedCompileResultExampleContract);
+        })
+
+        it('Should NOT compile contracts with --compiler argument - invalid one ', async () => {
+            let result = await execute(constants.cliCommands.COMPILE, ["--compiler", "compiler.somewhere.com"], executeOptions)
+            
+            assert.include(result, expectedResult4);
+        })
+    })
+
+    after(async () => {
+        await execute(constants.cliCommands.NODE, [constants.cliCommandsOptions.STOP], executeOptions)
+        fs.removeSync(`.${ constants.compileTestsFolderPath }`);
+    })
 })
