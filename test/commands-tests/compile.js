31425f179255b2f9bb4511b6ed5e0d1b98d77dc1
Martin Grigorov <maptuhec@gmail.com>
2019-08-21 17:00:12 +0300

    fix travis

--- test/commands-tests/compile.js
+++ test/commands-tests/compile.js
@@ -19,7 +19,7 @@ chai.use(chaiAsPromised);
 const LOCAL_COMPILER_URL = constants.LOCAL_COMPILER_URL;
 const INVALID_COMPILER_URL = 'https://compiler.somewhere.com';
 
-describe.only('AEproject Compile', () => {
+describe('AEproject Compile', () => {
     before(async () => {
         fs.ensureDirSync(`.${ constants.compileTestsFolderPath }`)
         await execute(constants.cliCommands.INIT, [], executeOptions)
