c151c6a65c74753864154b36f845bb928f491203
HristiyanG <Hristiyan@limechain.tech>
2019-10-07 15:56:44 +0300

    check default version of docker-compose

--- test/commands-tests/compile.js
+++ test/commands-tests/compile.js
@@ -5,7 +5,7 @@ const execute = require('../../packages/aeproject-utils/utils/aeproject-utils.js
 const fs = require('fs-extra')
 const constants = require('../constants.json')
 const expectedCompileResultExampleContract = "ExampleContract.aes has been successfully compiled'"
-
+const dockerExec = require('../../packages/aeproject-utils/utils/aeproject-utils.js').execute;
 let expectedResult1 = "ExampleContract1.aes has been successfully compiled"
 let expectedResult2 = "ExampleContract2.aes has been successfully compiled"
 let expectedResult3 = "ExampleContract3.aes has been successfully compiled"
@@ -19,11 +19,15 @@ chai.use(chaiAsPromised);
 const LOCAL_COMPILER_URL = constants.LOCAL_COMPILER_URL;
 const INVALID_COMPILER_URL = 'https://compiler.somewhere.com';
 
-describe('AEproject Compile', () => {
+describe.only('AEproject Compile', () => {
     before(async () => {
         fs.ensureDirSync(`.${ constants.compileTestsFolderPath }`)
         await execute(constants.cliCommands.INIT, [], executeOptions)
         await execute(constants.cliCommands.NODE, [], executeOptions)
+        let test = dockerExec('docker-compose', ['--version'])
+        console.log('docker compose version: ');
+        console.log(test);
+        
     })
 
     describe('Compile', () => {
