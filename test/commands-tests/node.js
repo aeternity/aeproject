972534b778ecaeff33a0b58c899f49cc8ee7ab71
HristiyanG <Hristiyan@limechain.tech>
2019-10-25 15:57:29 +0300

    removed chdir in the test

--- test/commands-tests/node.js
+++ test/commands-tests/node.js
@@ -67,8 +67,8 @@ describe("AEproject Node and Compiler Tests", async () => {
 
         it.only('Should start the node successfully', async () => {
             // We need to change directory where docker-compose config is located, so we can gather proper information for the node
-            await linkLocalUtilsToProject()
-            process.chdir(path.resolve(nodeTestDir))
+            // await linkLocalUtilsToProject()
+            // process.chdir(path.resolve(nodeTestDir))
             
             let running = await waitForContainer(waitForContainerOpts.dockerImage);
 
