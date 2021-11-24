fb780c608478075bf168cdeac182ac4c33c488f7
Philipp Piwo <github@piwo.space>
2021-11-24 17:19:28 +0100

    feat(env): abort tests if env is not running

--- src/init/artifacts/test/exampleTests.js
+++ src/init/artifacts/test/exampleTests.js
@@ -31,7 +31,7 @@ describe('ExampleContract', () => {
 
   it('ExampleContract: set and get', async () => {
     const set = await contract.methods.set(42);
-    assert.equal(set.decodedEvents[0].name, 'SetXEvent')
+    assert.equal(set.decodedEvents[0].name, 'SetXEvent');
     assert.equal(set.decodedEvents[0].decoded[0], wallets[0].publicKey);
     assert.equal(set.decodedEvents[0].decoded[1], 42);
 
