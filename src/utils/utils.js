1f0542a8dff60b133fa7287631961a02c7282897
Philipp Piwo <github@piwo.space>
2022-02-13 22:34:48 +0100

    chore(tests): improved env to wait for node to come up

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,22 @@
+const axios = require('axios');
+
 const config = require('../config/config.json');
+
+// eslint-disable-next-line no-promise-executor-return
+const pause = async (duration) => new Promise((resolve) => setTimeout(resolve, duration));
+
+const awaitNodeAvailable = async (interval = 200, attempts = 100) => {
+  let result = null;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < attempts; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    if (i) await pause(interval);
+    // eslint-disable-next-line no-await-in-loop
+    result = await axios.get('http://localhost:3001/v3/status').catch(() => null);
+    if (result) return;
+  }
+  throw new Error('timed out waiting for node to come up');
+};
 
 const getNetwork = (network) => {
   const networks = {
@@ -26,4 +44,5 @@ module.exports = {
   printError: console.error,
   config,
   getNetwork,
+  awaitNodeAvailable,
 };
