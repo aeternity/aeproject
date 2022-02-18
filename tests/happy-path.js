6ba0e2f5c23c4d1392d70abf519f0500d1f3a7ec
Philipp Piwo <github@piwo.space>
2022-02-18 12:10:04 +0100

    test: improved exec env, assert file existence

--- tests/happy-path.js
+++ tests/happy-path.js
@@ -1,11 +1,19 @@
 const path = require('path');
-const { exec } = require('promisify-child-process');
+const { exec: execP } = require('promisify-child-process');
 const fs = require('fs');
+const chai = require('chai');
+const chaiFiles = require('chai-files');
+
+chai.use(chaiFiles);
+const { assert } = chai;
+const { file } = chaiFiles;
 
 const cwd = path.join(process.cwd(), '.testdir');
+const exec = (cmd, options) => execP(`source ~/.profile;${cmd}`, options);
 
 describe('Happy Path', () => {
   before(async () => {
+    await exec('npm link');
     if (!fs.existsSync(cwd)) fs.mkdirSync(cwd);
   });
 
@@ -15,6 +23,16 @@ describe('Happy Path', () => {
 
   it('init', async () => {
     await exec('aeproject init', { cwd });
+
+    assert.exists(file(path.join(cwd, '.gitignore')));
+    assert.exists(file(path.join(cwd, 'docker-compose.yml')));
+    assert.exists(file(path.join(cwd, 'package.json')));
+
+    assert.exists(file(path.join(cwd, 'contracts/ExampleContract.aes')));
+    assert.exists(file(path.join(cwd, 'docker/accounts.json')));
+    assert.exists(file(path.join(cwd, 'docker/aeternity.yaml')));
+    assert.exists(file(path.join(cwd, 'docker/nginx.conf')));
+    assert.exists(file(path.join(cwd, 'test/exampleTest.js')));
   });
 
   it('env', async () => {
