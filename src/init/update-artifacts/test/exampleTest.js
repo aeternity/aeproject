526cb402deaadaff4bd4676d6b85c50053cd5e01
Philipp Piwo <github@piwo.space>
2022-06-18 15:30:38 +0200

    feat: fix breaking changes for sdk 12 usage

--- src/init/update-artifacts/test/exampleTest.js
+++ src/init/update-artifacts/test/exampleTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { utils, wallets } = require('@aeternity/aeproject');
+const { utils } = require('@aeternity/aeproject');
 
 const EXAMPLE_CONTRACT_SOURCE = './contracts/ExampleContract.aes';
 
@@ -11,13 +11,13 @@ describe('ExampleContract', () => {
     aeSdk = await utils.getSdk();
 
     // a filesystem object must be passed to the compiler if the contract uses custom includes
-    const filesystem = utils.getFilesystem(EXAMPLE_CONTRACT_SOURCE);
+    const fileSystem = utils.getFilesystem(EXAMPLE_CONTRACT_SOURCE);
 
     // get content of contract
     const source = utils.getContractContent(EXAMPLE_CONTRACT_SOURCE);
 
     // initialize the contract instance
-    contract = await aeSdk.getContractInstance({ source, filesystem });
+    contract = await aeSdk.getContractInstance({ source, fileSystem });
     await contract.deploy();
 
     // create a snapshot of the blockchain state
@@ -30,9 +30,9 @@ describe('ExampleContract', () => {
   });
 
   it('ExampleContract: set and get', async () => {
-    const set = await contract.methods.set(42, { onAccount: wallets[1].publicKey });
+    const set = await contract.methods.set(42, { onAccount: utils.getDefaultAccounts()[1] });
     assert.equal(set.decodedEvents[0].name, 'SetXEvent');
-    assert.equal(set.decodedEvents[0].args[0], wallets[1].publicKey);
+    assert.equal(set.decodedEvents[0].args[0], await utils.getDefaultAccounts()[1].address());
     assert.equal(set.decodedEvents[0].args[1], 42);
 
     const { decodedResult } = await contract.methods.get();
