dddd2a06bbe2b2b7578f35cc31c29594c3f666eb
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-12-20 20:17:26 +0100

    refactor: remove unused `config`, `getNetwork` in internal utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,28 +1,2 @@
-import { readFile } from "fs/promises";
-
-const config = JSON.parse(
-  await readFile(new URL("../config/config.json", import.meta.url)),
-);
-
-export function getNetwork(network) {
-  const networks = {
-    local: {
-      url: config.localhostParams.url,
-      networkId: config.localhostParams.networkId,
-    },
-    testnet: {
-      url: config.testNetParams.url,
-      networkId: config.testNetParams.networkId,
-    },
-    mainnet: {
-      url: config.mainNetParams.url,
-      networkId: config.mainNetParams.networkId,
-    },
-  };
-
-  return networks[network];
-}
-
 export const print = console.log;
 export const printError = console.error;
-export { config };
